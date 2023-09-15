d0f52e046209840c88be7c2f52fc560514bf7563
tedblair2 <39332527+tedblair2@users.noreply.github.com>
2023-09-15 17:18:19 +0300

    changed channel id and channel name values

--- app/src/main/java/com/example/compose2/Util.kt
+++ app/src/main/java/com/example/compose2/Util.kt
@@ -1,8 +1,8 @@
 package com.example.compose2
 
 object Util {
-    const val CHANNEL_ID="${R.string.app_name}"
-    const val CHANNEL_NAME="${R.string.app_name}"
+    const val CHANNEL_ID="Muziki"
+    const val CHANNEL_NAME="Muziki"
     const val NOTIFICATION_ID=24
     const val stop_action="stop_action"
 }
